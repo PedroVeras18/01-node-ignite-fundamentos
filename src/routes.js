4e82af2469d74ee6a9c71df0e75d9d838777b40c
Pedro Veras <veraspedro1608@gmail.com>
2023-04-01 21:58:57 -0300

    Rotas da aplicação: Route e Query parameters

--- src/routes.js
+++ src/routes.js
@@ -4,30 +4,37 @@ import { Database } from './database.js'
 const database = new Database()
 
 export const routes = [
-  {
-    method: 'GET',
-    path: '/users',
-    handler: (req, res) => {
-      const users = database.select('users')
+    {
+        method: 'GET',
+        path: '/users',
+        handler: (req, res) => {
+            const users = database.select('users')
 
-      return res.end(JSON.stringify(users))
+            return res.end(JSON.stringify(users))
+        }
+    },
+    {
+        method: 'POST',
+        path: '/users',
+        handler: (req, res) => {
+            const { name, email } = req.body
+
+            const user = {
+                id: randomUUID(),
+                name,
+                email,
+            }
+
+            database.insert('users', user)
+
+            return res.writeHead(201).end()
+        }
+    },
+    {
+        method: 'DELETE',
+        path: '/users/ID',
+        handler: (req, res) => {
+            return res.end()
+        }
     }
-  },
-  {
-    method: 'POST',
-    path: '/users',
-    handler: (req, res) => {
-      const { name, email } = req.body
-
-      const user = {
-        id: randomUUID(),
-        name,
-        email,
-      }
-
-      database.insert('users', user)
-
-      return res.writeHead(201).end()
-    }
-  },
 ]
