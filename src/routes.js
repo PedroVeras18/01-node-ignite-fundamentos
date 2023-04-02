6aa7fca4b2be93c4ef8d73f6b18a4fa1e6c691e9
Pedro Veras <veraspedro1608@gmail.com>
2023-04-01 22:09:05 -0300

    Rotas da aplicação: filtrando lista do banco de dados

--- src/routes.js
+++ src/routes.js
@@ -9,9 +9,12 @@ export const routes = [
         method: 'GET',
         path: buildRoutePath('/users'),
         handler: (req, res) => {
-            console.log(req.query)
+            const { search } = req.query
 
-            const users = database.select('users')
+            const users = database.select('users', search ? {
+                name: search,
+                email: search
+            } : null)
 
             return res.end(JSON.stringify(users))
         }
@@ -37,17 +40,17 @@ export const routes = [
         method: 'PUT',
         path: buildRoutePath('/users/:id'),
         handler: (req, res) => {
-          const { id } = req.params
-          const { name, email } = req.body
-    
-          database.update('users', id, {
-            name,
-            email,
-          })
-    
-          return res.writeHead(204).end()
+            const { id } = req.params
+            const { name, email } = req.body
+
+            database.update('users', id, {
+                name,
+                email,
+            })
+
+            return res.writeHead(204).end()
         }
-      },
+    },
     {
         method: 'DELETE',
         path: buildRoutePath('/users/:id'),
@@ -55,7 +58,7 @@ export const routes = [
             const { id } = req.params
 
             database.delete('users', id)
-      
+
             return res.writeHead(204).end()
         }
     }
--- streams/fake-upload-to-http-stream.js
+++ streams/fake-upload-to-http-stream.js
@@ -1,28 +1,28 @@
 import { Readable } from 'node:stream'
 
 class OneToHundredStream extends Readable {
-  index = 1
+    index = 1
 
-  _read() {
-    const i = this.index++
+    _read() {
+        const i = this.index++
 
-    setTimeout(() => {
-      if (i > 5) {
-        this.push(null)
-      } else {
-        const buf = Buffer.from(String(i))
+        setTimeout(() => {
+            if (i > 5) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
 
-        this.push(buf)
-      }
-    }, 1000);
-  }
+                this.push(buf)
+            }
+        }, 1000);
+    }
 }
 
 fetch('http://localhost:3334', {
-  method: 'POST',
-  body: new OneToHundredStream(),
+    method: 'POST',
+    body: new OneToHundredStream(),
 }).then(res => {
-  return res.text()
+    return res.text()
 }).then(data => {
-  console.log(data)
+    console.log(data)
 })
